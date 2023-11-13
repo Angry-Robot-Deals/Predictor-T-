02a10eaa48a7d9431e90af5e3f22f5e3a106cbf6
Aleksey Savin <saleksey67@gmail.com>
2023-11-13 16:26:55 +0300

    ..

--- TradingFlow/src/integration/src/lib.rs
+++ TradingFlow/src/integration/src/lib.rs
@@ -1,9 +1,20 @@
 use pyo3::prelude::*;
+use ccxt::exchange::{Exchange, Value, normalize, ValueTrait};
+use ccxt::binance::{Binance, BinanceImpl};
+
+use serde_json::json;
+const UNDEFINED: Value = Value::Undefined;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
+}
+
+
+#[pyfunction]
+fn call_rust_sleep(py: Python) -> PyResult<String> {
+    Ok("call_rust_sleep".to_string())
 }
 
 /// A Python module implemented in Rust.
